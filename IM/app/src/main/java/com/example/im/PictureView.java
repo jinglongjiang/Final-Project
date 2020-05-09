7b7ebf914a7bf9439b5f02f0ae80b3347e51aa8d
jinglongjiang <a01077887384@gmail.com>
2020-05-10 01:09:09 +0900

    doing the async about upload images to server

--- IM/app/src/main/java/com/example/im/PictureView.java
+++ IM/app/src/main/java/com/example/im/PictureView.java
@@ -1,22 +1,35 @@
 package com.example.im;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.squareup.okhttp.MediaType;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.RequestBody;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.util.Map;
+import java.io.IOException;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class PictureView extends AppCompatActivity {
+    private String result;
     private Uri photoUri;
+    private boolean success = false;
+    private String selectedImagePath;
+    private static final MediaType MEDIA_TYPE_PNG = MediaType.parse("image/png");
     ImageView imageView;
 
     @Override
@@ -28,31 +41,69 @@ public class PictureView extends AppCompatActivity {
         imageView = (ImageView)findViewById(R.id.img);
 
         imageView.setImageURI(photoUri);
-
-        String url = "127.0.0.1:8000/upload";
-
+        connectServer();
     }
 
-    private void uploadimg(Uri uri){
-        new Thread(new Runnable() {
+    void connectServer(){
+
+        String postUrl= "127.0.0.1:8000/upload";
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inPreferredConfig = Bitmap.Config.RGB_565;
+        // Read BitMap by file path
+        Bitmap bitmap = BitmapFactory.decodeFile(selectedImagePath, options);
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
+
+        RequestBody postBodyImage = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("image", "androidFlask.jpg", RequestBody.create(okhttp3.MediaType.parse("image/*jpg"), byteArray))
+                .build();
+
+        uploadimg(postUrl, postBodyImage);
+    }
+
+    private void uploadimg(String postUrl, RequestBody postBody){
+        okhttp3.OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(postUrl)
+                .post(postBody)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
             @Override
-            public void run() {
-                OkHttpClient client = new OkHttpClient();
-                MultipartBody.Builder builder1 = new MultipartBody.Builder();
-                builder1.setType(MultipartBody.FORM);
-                for (Map.Entry<String, Object> stringObjectEntry : params.entrySet()) {//没有判空
-                    String key = stringObjectEntry.getKey();
-                    Object value = stringObjectEntry.getValue();
-                    if (value instanceof File) {//如果请求的值是文件
-                        File file = (File) value;
-                        //MediaType.parse("application/octet-stream")以二进制的形式上传文件
-                        builder1.addFormDataPart("jokeFiles", ((File) value).getName(), RequestBody.create(MediaType.parse("application/octet-stream"), file));
-                    } else {//如果请求的值是string类型
-                        if(!"url".equals(key)){
-                            builder1.addFormDataPart(key, value.toString());
+            public void onFailure(Call call, IOException e) {
+                // Cancel the post on failure.
+                call.cancel();
+
+                // In order to access the TextView inside the UI thread, the code is executed inside runOnUiThread()
+                /*
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //TextView responseText = findViewById(R.id.loadingTextview);
+                        responseText.setText("Failed to Connect to Server");
+                    }
+                });*/
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                // In order to access the TextView inside the UI thread, the code is executed inside runOnUiThread()
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            result = response.body().string();
+                            success = true;
+                            System.out.println(result);
+                        } catch (IOException e) {
+                            e.printStackTrace();
                         }
                     }
-                }
+                });
             }
         });
     }
