bd7ef925ed13cbfe1ad15be9addead15357a3d07
jinglongjiang <a01077887384@gmail.com>
2020-05-09 23:53:10 +0900

    sever file pull

--- IM/app/src/main/java/com/example/im/PictureView.java
+++ IM/app/src/main/java/com/example/im/PictureView.java
@@ -6,7 +6,14 @@ import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-//import com.androidquery.AQuery;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.RequestBody;
+
+import java.io.File;
+import java.util.Map;
+
+import okhttp3.MultipartBody;
 
 public class PictureView extends AppCompatActivity {
     private Uri photoUri;
@@ -23,6 +30,30 @@ public class PictureView extends AppCompatActivity {
         imageView.setImageURI(photoUri);
 
         String url = "127.0.0.1:8000/upload";
-        //final AQuery aq=new AQuery(MainActivity.this);
+
+    }
+
+    private void uploadimg(Uri uri){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                OkHttpClient client = new OkHttpClient();
+                MultipartBody.Builder builder1 = new MultipartBody.Builder();
+                builder1.setType(MultipartBody.FORM);
+                for (Map.Entry<String, Object> stringObjectEntry : params.entrySet()) {//没有判空
+                    String key = stringObjectEntry.getKey();
+                    Object value = stringObjectEntry.getValue();
+                    if (value instanceof File) {//如果请求的值是文件
+                        File file = (File) value;
+                        //MediaType.parse("application/octet-stream")以二进制的形式上传文件
+                        builder1.addFormDataPart("jokeFiles", ((File) value).getName(), RequestBody.create(MediaType.parse("application/octet-stream"), file));
+                    } else {//如果请求的值是string类型
+                        if(!"url".equals(key)){
+                            builder1.addFormDataPart(key, value.toString());
+                        }
+                    }
+                }
+            }
+        });
     }
 }
--- server/server/settings.py
+++ server/server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ufa9tzy8ezwok$1x_67$oqv5%k2=tam)&a&k1sx_x_x5+h88eo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,7 +43,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
