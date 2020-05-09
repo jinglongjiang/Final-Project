517cdf0659573fcc4a89fcc05363e9aeef644b69
jinglongjiang <a01077887394@gmail.com>
2020-05-10 01:07:03 +0900

    doing the remake from model to server file

--- server/server/urls.py
+++ server/server/urls.py
@@ -18,5 +18,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('',views.admin),
     path('upload',views.upload)
 ]
--- server/server/views.py
+++ server/server/views.py
@@ -1,13 +1,37 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators import csrf
- 
-def homepage(request):
-    return render(request,'homepage.html')
+from PIL import Image
+import numpy as np
+import flask
+from flask import render_template, redirect
+from flask import request, url_for, render_template, redirect
+
+def admin(request):
+
+        
+    return render(request,'test.html')
 
 def upload(request):
     file = request.FILES.get('file')
+    image = image1.save(os.path.join(app.config['UPLOAD_FOLDER'], image1.filename)) 
+    with open(os.path.join(app.config['UPLOAD_FOLDER'], image1.filename), 'rb') as f:
+    image = Image.open(io.BytesIO(f.read()))        
+    processed_image = prepare_image(image, target=(224, 224))
+
+            global sess
+            global graph
+            with graph.as_default():
+                set_session(sess)
+                preds = model.predict(processed_image)
+                results = imagenet_utils.decode_predictions(preds)
+                data["predictions"] = []
+
+            data["success"] = "Uploaded"
+            title = "predict"
+            
     with open(file.name,'wb') as f:
         for line in file:
             f.write(line)
-    return HttpResponse('succed')
+    a = '345'
+    return HttpResponse(a)
