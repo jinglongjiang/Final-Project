dba7bd6e35fd75d0b109ede86de1670870fc81c6
jinglongjiang <a01077887384@gmail.com>
2020-05-10 16:50:10 +0900

    finish the async image upload and get response

--- IM/app/src/main/java/com/example/im/PictureView.java
+++ IM/app/src/main/java/com/example/im/PictureView.java
@@ -1,36 +1,54 @@
 package com.example.im;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.Header;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import com.androidquery.AQuery;
+import com.androidquery.callback.AjaxCallback;
+import com.androidquery.callback.AjaxStatus;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 import com.squareup.okhttp.MediaType;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
+import org.json.JSONArray;
 
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.MultipartBody;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+
 
 public class PictureView extends AppCompatActivity {
     private String result;
     private Uri photoUri;
+    private String srcPath;
     private boolean success = false;
-    private String selectedImagePath;
-    private static final MediaType MEDIA_TYPE_PNG = MediaType.parse("image/png");
     ImageView imageView;
+    TextView textView;
+    private String url = "http://10.0.2.2:8000/upload";
+    private final int REQUEST_EXTERNAL_STORAGE = 1;
+    private  String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,73 +56,82 @@ public class PictureView extends AppCompatActivity {
         setContentView(R.layout.pictures_view);
 
         photoUri = getIntent().getParcelableExtra("photo");
+        srcPath = getPath(photoUri);
         imageView = (ImageView)findViewById(R.id.img);
 
         imageView.setImageURI(photoUri);
+        verifyStoragePermissions(this);
         connectServer();
     }
 
-    void connectServer(){
+    private void connectServer() {
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
 
-        String postUrl= "127.0.0.1:8000/upload";
+        try {
+            params.put("file",new File(srcPath));
 
-        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inPreferredConfig = Bitmap.Config.RGB_565;
-        // Read BitMap by file path
-        Bitmap bitmap = BitmapFactory.decodeFile(selectedImagePath, options);
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
-        byte[] byteArray = stream.toByteArray();
+            hasSdcard();
+            client.post(url, params, new AsyncHttpResponseHandler() {
 
-        RequestBody postBodyImage = new MultipartBody.Builder()
-                .setType(MultipartBody.FORM)
-                .addFormDataPart("image", "androidFlask.jpg", RequestBody.create(okhttp3.MediaType.parse("image/*jpg"), byteArray))
-                .build();
+                @Override
+                public void onFailure(int arg0, Header[] arg1, byte[] arg2,
+                                      Throwable arg3) {
+                    System.out.println(arg0);
+                }
 
-        uploadimg(postUrl, postBodyImage);
+                @Override
+                public void onSuccess(int arg0, Header[] arg1, byte[] arg2) {
+                    try {
+                        result = new String(arg2,"UTF-8");
+                        textView = findViewById(R.id.type);
+                        textView.setText(result);
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    private void uploadimg(String postUrl, RequestBody postBody){
-        okhttp3.OkHttpClient client = new OkHttpClient();
+    public String getPath(Uri uri){
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+        cursor.moveToFirst();
+        String document_id = cursor.getString(0);
+        document_id = document_id.substring(document_id.lastIndexOf(":")+1);
+        cursor.close();
 
-        Request request = new Request.Builder()
-                .url(postUrl)
-                .post(postBody)
-                .build();
+        Log.d("!@#!@#", "ASDF");
+        cursor = getContentResolver().query(
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                null, MediaStore.Images.Media._ID + " = ? ", new String[]{document_id}, null);
+        cursor.moveToFirst();
+        String path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+        cursor.close();
 
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                // Cancel the post on failure.
-                call.cancel();
+        return path;
+    }
 
-                // In order to access the TextView inside the UI thread, the code is executed inside runOnUiThread()
-                /*
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        //TextView responseText = findViewById(R.id.loadingTextview);
-                        responseText.setText("Failed to Connect to Server");
-                    }
-                });*/
-            }
+    private boolean hasSdcard() {
+        //判断ＳＤ卡手否是安装好的　　　media_mounted
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-            @Override
-            public void onResponse(Call call, final Response response) throws IOException {
-                // In order to access the TextView inside the UI thread, the code is executed inside runOnUiThread()
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            result = response.body().string();
-                            success = true;
-                            System.out.println(result);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
-            }
-        });
+    public void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE);
+        }
     }
 }
